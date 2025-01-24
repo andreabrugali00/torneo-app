9937cf5e917360eb26de135465d99e35fa2a7575
Vijaye Raji <vijaye@statsig.com>
2025-01-24 16:08:48 +0100

    Sostituito frontend Vue con React + Vite

--- frontend/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const apiClient  = axios.create({
-  baseURL: 'http://localhost:3000', // URL del tuo backend Express
-  timeout: 5000,
-});
-
-export default apiClient;
